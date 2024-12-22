a7dac66877ad3de58ae15557dbc6351c56c0aec8
Robert Shen <rb.shen@gmail.com>
2024-12-22 19:51:40 +0800

    Tailwind CSS Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <h2 className="text-3xl font-black text-white">
+    <h2 className="text-3xl font-black text-violet-700">
       Welcome to the world of Next.js
     </h2>
   );
